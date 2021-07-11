cef0ea6ccb0dc036ae2f515437e17ba646539ad3
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-07-11 17:57:01 +0530

    chore: add view templates, list routes

--- src/web.lisp
+++ src/web.lisp
@@ -20,11 +20,28 @@
 (defvar *web* (make-instance '<web>))
 (clear-routing-rules *web*)
 
+;; Boards
+(defvar *board* '((:name "To Do" :id 1 :cards ((:id 1 :label "First Card" :list 1)
+                                                (:id 2 :label "Second Card" :list 1)))
+                   (:name "Doing" :id 2 :cards ((:id 1 :label "First Card" :list 2)
+                                                (:id 2 :label "Second Card" :list 2)))))
+
 ;;
 ;; Routing rules
 
 (defroute "/" ()
-  (render #P"index.html"))
+  (render #P"index.html" (list :board *board*)))
+
+(defroute "/lists/add" ()
+  (render #P"_add-list.html"))
+
+(defroute "/lists/cancel" ()
+  (render #P"_new-list.html"))
+
+(defroute ("/lists" :method :POST) (&key _parsed)
+  (let ((name (cdr (assoc "name" _parsed :test #'string=))))
+    (push (list :name name :id (+ 1 (length *board*)) :cards ()) *board*)
+    (render #P"_board.html" (list :board (reverse *board*)))))
 
 ;;
 ;; Error pages
